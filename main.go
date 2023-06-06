5fe7e16c6be28db4756b235899a3848984c88922
aleroxac <acardoso.ti@gmail.com>
2023-06-05 22:35:41 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/aleroxac/gopportunities/router"
+import (
+	"github.com/aleroxac/gopportunities/config"
+	"github.com/aleroxac/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// init configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// init router
 	router.Initialize()
 }
--- router/routes.go
+++ router/routes.go
@@ -8,10 +8,10 @@ import (
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/openings", handler.GetOpeningHandler)
-		v1.POST("/openings", handler.CreateOpeningHandler)
-		v1.DELETE("/openings", handler.DeleteOpeningHandler)
-		v1.PUT("/openings", handler.UpdateOpeningHandler)
+		v1.GET("/opening", handler.GetOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
 }
